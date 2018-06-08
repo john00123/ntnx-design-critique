141d7c57f31f45d1101eb906ec4788797d73fc3b
john <john.torres.hurtado@gmail.com>
2018-06-07 23:04:03 -0700

    added firebase

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAJe0L2RZg3NsG_DQWbfdnZhJTaap18icE",
+    authDomain: "design-critiques.firebaseapp.com",
+    databaseURL: "https://design-critiques.firebaseio.com",
+    projectId: "design-critiques",
+    storageBucket: "",
+    messagingSenderId: "1053289515982"
+  };
+  firebase.initializeApp(config);
