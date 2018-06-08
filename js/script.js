ccc3fe0695d50fa89b799e8b9d4abe6d109db46c
john <john.torres.hurtado@gmail.com>
2018-06-07 23:09:11 -0700

    move firebase

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var config = {
-    apiKey: "AIzaSyAJe0L2RZg3NsG_DQWbfdnZhJTaap18icE",
-    authDomain: "design-critiques.firebaseapp.com",
-    databaseURL: "https://design-critiques.firebaseio.com",
-    projectId: "design-critiques",
-    storageBucket: "",
-    messagingSenderId: "1053289515982"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyAJe0L2RZg3NsG_DQWbfdnZhJTaap18icE",
+  authDomain: "design-critiques.firebaseapp.com",
+  databaseURL: "https://design-critiques.firebaseio.com",
+  projectId: "design-critiques",
+  storageBucket: "",
+  messagingSenderId: "1053289515982"
+};
+firebase.initializeApp(config);
+
+console.log(firebase);
